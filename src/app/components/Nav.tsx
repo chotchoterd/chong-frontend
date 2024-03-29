eaffa4d9c5fe2ac9e5b2c34e15625ecf40a02c1c
Jirayu Aunkhonthee <126430616+chotchoterd@users.noreply.github.com>
2024-03-29 11:31:14 +0700

    commit

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,14 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import React from "react";
 
 function Nav() {
   return (
-    <ul className="flex justify-center p-4 bg-sky-800">
-      <li className="mx-3 rounded hover:bg-gray-200 py-1 px-3 text-white hover:text-black font-bold"><Link style={{ fontSize: '18px' }} href="#">Home</Link></li>
-      <li className="mx-3 rounded hover:bg-gray-200 py-1 px-3 text-white hover:text-black font-bold"><Link style={{ fontSize: '18px' }} href="#">Services</Link></li>
-      <li className="mx-3 rounded hover:bg-gray-200 py-1 px-3 text-white hover:text-black font-bold"><Link style={{ fontSize: '18px' }} href="#">About</Link></li>
-      <li className="mx-3 rounded hover:bg-gray-200 py-1 px-3 text-white hover:text-black font-bold"><Link style={{ fontSize: '18px' }} href="#">Contact</Link></li>
-    </ul>
+    <div className="w-full max-w-screen-xl mx-auto p-3 md:py-4 bg-sky-800">
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <a href="./" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+          <img src="/food.png" className="h-16" alt="food" />
+          <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-white text-white">CHONG</span>
+        </a>
+        <ul className="flex  p-4 bg-sky-800">
+          <li className="mx-3 rounded hover:bg-gray-200 py-1 px-3 text-white hover:text-black font-bold"><Link style={{ fontSize: '18px' }} href="#">Home</Link></li>
+          <li className="mx-3 rounded hover:bg-gray-200 py-1 px-3 text-white hover:text-black font-bold"><Link style={{ fontSize: '18px' }} href="#">Services</Link></li>
+          <li className="mx-3 rounded hover:bg-gray-200 py-1 px-3 text-white hover:text-black font-bold"><Link style={{ fontSize: '18px' }} href="#">About</Link></li>
+          <li className="mx-3 rounded hover:bg-gray-200 py-1 px-3 text-white hover:text-black font-bold"><Link style={{ fontSize: '18px' }} href="#">Contact</Link></li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
